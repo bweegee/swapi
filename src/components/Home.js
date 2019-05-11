43449a26f9dc0511169af91276f13051ddc78bab
bweegee <bw33g33@gmail.com>
2019-05-11 11:54:36 -0600

    Screenshots for readMe

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
-import React, { Fragment } from 'react'
-import logo from '../images/SWLogo.png'
+import React from 'react'
 import styled from 'styled-components'
 
 export default function Home() {
   return (
     <Div>
-      <img src={logo} alt='star wars logo' />
-      <h1>API</h1>
+      <Header>Star</Header>
+      <Header>Wars</Header>
+      <Header>api</Header>
     </Div>
   )
 }
 
-const Div = styled.div`
+const Header = styled.h1`
   color: #FFE81F;
-  margin: auto;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-family: STJEDISE
+  text-align: center;
+`
+const Div = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  font-size: 3rem;
 `
